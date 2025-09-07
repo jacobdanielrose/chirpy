ec32a55b7ff01b58fbdc90b23efd81f94dff54aa
Jacob Rose <jacobdanielrose@protonmail.com>
2025-09-07 17:39:09 +0200

    added query support for filtering chirps by author

--- handler_chirps.go
+++ handler_chirps.go
@@ -24,16 +24,28 @@ func (cfg *apiConfig) handlerGetChirps(w http.ResponseWriter, r *http.Request) {
 		respondWithError(w, http.StatusInternalServerError, "Coudln't retrieve chirps", err)
 	}
 
-	chirps := make([]Chirp, len(dbChirps))
-
-	for i, chirp := range dbChirps {
-		chirps[i] = Chirp{
-			ID:        chirp.ID,
-			CreatedAt: chirp.CreatedAt,
-			UpdatedAt: chirp.UpdatedAt,
-			Body:      chirp.Body,
-			UserID:    chirp.UserID,
+	authorID := uuid.Nil
+	authorIDString := r.URL.Query().Get("author_id")
+	if authorIDString != "" {
+		authorID, err = uuid.Parse(authorIDString)
+		if err != nil {
+			respondWithError(w, http.StatusBadRequest, "Invalid author ID", err)
+			return
 		}
+	}
+
+	chirps := []Chirp{}
+	for _, dbChirp := range dbChirps {
+		if authorID != uuid.Nil && dbChirp.UserID != authorID {
+			continue
+		}
+		chirps = append(chirps, Chirp{
+			ID:        dbChirp.ID,
+			CreatedAt: dbChirp.CreatedAt,
+			UpdatedAt: dbChirp.UpdatedAt,
+			Body:      dbChirp.Body,
+			UserID:    dbChirp.UserID,
+		})
 	}
 
 	respondWithJSON(w, http.StatusOK, chirps)
